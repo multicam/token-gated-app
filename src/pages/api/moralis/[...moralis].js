1d569e06daec05fa20ecae3f18de2f7f96dfed0e
Jean-Marc Giorgi <jmgiorgi@gmail.com>
2023-09-24 14:08:48 +1000

    Update [...moralis].js

--- src/pages/api/moralis/[...moralis].js
+++ src/pages/api/moralis/[...moralis].js
@@ -3,7 +3,7 @@ import { MoralisNextApi } from "@moralisweb3/next";
 export default MoralisNextApi({
     apiKey: process.env.MORALIS_API_KEY,
     authentication: {
-        domain: "amazing.dapp",
+        domain: "token-gated-demo.vercel.app",
         uri: process.env.NEXTAUTH_URL,
         timeout: 120,
     },
