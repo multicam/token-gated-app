eae7be2c3e895f0887e9fa9781d3a8e8c7e3533b
multicam <jmgiorgi@gmail.com>
2023-09-29 18:18:34 +1000

    -- elaborating /2

--- src/pages/api/moralis/[...moralis].js
+++ src/pages/api/moralis/[...moralis].js
@@ -3,14 +3,8 @@ import {MoralisNextApi} from "@moralisweb3/next";
 export default MoralisNextApi({
     apiKey: process.env.MORALIS_API_KEY,
     authentication: {
-        domain:
-            // process.env.NODE_ENV === 'production' ?
-            //     'token-gated-app.vercel.app' :
-                'localhost:3000',
-        uri:
-        //     process.env.NODE_ENV === 'production' ?
-        //         'https://token-gated-app.vercel.app' :
-                'http://localhost:3000',
+        domain: process.env.NEXTAUTH_URL.replace('https://','').replace('http://',''),
+        uri: process.env.NEXTAUTH_URL,
         timeout: 120,
     },
 });
